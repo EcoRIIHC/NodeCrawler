d52ac482d1e96cad34838d728f775c08e07525a4
luhaichuan <luhaichuan@baidu.com>
2017-06-07 09:51:46 +0800

    新增将歌曲信息插入到mysql数据库中；

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const request = require('request')
-
+const mysql = require('mysql')
 // 页面地址
 const url = 'http://music.163.com'
 const playListUrl = url + '/discover'
 const songListUrl = url + '/api/playlist/detail?id='
 const songDetailUrl = url + '/api/song/detail/?id=#{id}&ids=[#{id}]'
 const songHrefUrl = url + '/weapi/song/enhance/player/url?csrf_token='
+// let songDir = './songs/'
+// nginx下歌曲 路径
+const songDir = 'D:/WorkSpace/ReactPlayer/nginx-1.13.1/html/songs/'
+const songFile = './songs/song.txt'
+// 服务器地址
+const server = 'http://localhost/songs/'
 
-let songDir = './songs/'
-let songFile = './songs/song.txt'
 
 console.log('start processing')
 
@@ -43,10 +47,10 @@ superagent
             playList.push(tmp)
         })
         console.log('get song list .....done')
-        playList.forEach((item, i) => {
-            getSongs(item)
-        })
-        // getSongs(playList[1])
+        // playList.forEach((item, i) => {
+        //     getSongs(item)
+        // })
+        getSongs(playList[1])
     })
 // 根据播列表id获取下面的歌曲ids
 const getSongs = (item) => {
@@ -60,13 +64,38 @@ const getSongs = (item) => {
             playListInfo.result.tracks.forEach((item, i) => {
                 let tmp = {
                     id: item.id,
-                    name: item.name
+                    name: item.name,
+                    singer: ((item.artists || []).map((value) => {
+                        return value.name
+                    })).join(',') || '',
+                    album: item.album.name,
+                    time: ''
+                }
+                if (item.hMusic) {
+                    tmp.time = formatTime(item.hMusic.playTime)
+                } else if (item.mMusic) {
+                    tmp.time = formatTime(item.mMusic.playTime)
+                } else if (item.bMusic) {
+                    tmp.time = formatTime(item.bMusic.playTime)
+                } else if (item.lMusic) {
+                    tmp.time = formatTime(item.lMusic.playTime)
+                } else {
+                    tmp.time = formatTime(0)
                 }
                 songList.push(tmp)
             })
             console.log('获取播放列表' + item.title + '下的歌曲')
-            songList.forEach((value, i) => {
-                getSongHref (value, item.title)
+            var arr = songList.map((value, i) => {
+                return function () {
+                    getSongHref (value, item.title)
+                }
+                // getSongHref (value, item.title)
+            })
+            async.parallel(arr, (err, results) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.log('async........done')
             })
         })
 }
@@ -86,10 +115,16 @@ const getSongHref = (item, playlistTitle) => {
         .set('Cookies', 'os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; channel=netease; __remember_me=true')
         .set('Referer', 'http://music.163.com/')
         .end((err, res) => {
+            if (err) {
+                console.log(err)
+            }
             res = JSON.parse(res.text)
             let tmp = {
                 id: item.id,
                 name: item.name,
+                singer: item.singer,
+                time: item.time,
+                album: item.album,
                 url: res.data[0].url
             }
             songHref.push(tmp)
@@ -100,20 +135,57 @@ const getSongHref = (item, playlistTitle) => {
         })
 }
 
-// 下载歌曲
+// 连接数据库
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'react_player'
+
+})
+connection.connect()
+let sqlTpl = 'INSERT INTO songs (name, singer, time, album, url) VALUES("${name}", "${singer}", "${time}", "${album}", "${url}")'
+
+/**
+ * 下载歌曲
+ *
+ * @带url的歌曲对象 {any} item
+ * @播放列表名称，用于保存路径 {any} playlistTitle
+ */
 const download = (item, playlistTitle) => {
-    item.name = item.name
-        .replace(/\//g, '-')
-        .replace(/[:：|『』]/g, '-')
-    playlistTitle = playlistTitle
-        .replace(/\//g, '-')
-        .replace(/[:：|]/g, '-')
+    item.name = textFilter(item.name)
+    playlistTitle = textFilter(playlistTitle)
 
     if (!fs.existsSync(songDir + playlistTitle)) {
         fs.mkdirSync(songDir + playlistTitle)
     }
 
-    request(item.url).pipe(fs.createWriteStream(songDir + playlistTitle + '/' + item.name + '.mp3'))
+    request(item.url)
+        .on('error', (err) => {
+            console.log('下载 '+ item.name + '出错')
+            console.log(err)
+        })
+        .pipe(fs.createWriteStream(songDir + playlistTitle + '/' + item.name + '.mp3'))
+        .on('end', () => {
+            console.log('下载 ' + item.name + ' 完成')
+        })
+        .on('finish', () => {
+            console.log('歌曲 ' + item.name + ' 下载完成')
+
+            let sql = sqlTpl
+                .replace('${name}', item.name)
+                .replace('${singer}', item.singer)
+                .replace('${time}', item.time)
+                .replace('${album}', item.album)
+                .replace('${album}', item.album)
+                .replace('${url}', server + playlistTitle + '/' + item.name + '.mp3')
+                console.log(sql)
+            connection.query(sql, (err, result) => {
+                if(err) {
+                    console.log(err)
+                }
+            })
+        })
 }
 
 // 下载歌曲原生版本，原生的http.request没法下载重定向后的文件，先使用request代替了
@@ -144,6 +216,20 @@ const downloadOrigin = (item) => {
         console.log(err)
     })
     req.end()
+}
+
+// 过滤特殊字符
+const textFilter = (text) => {
+    return text
+        .replace(/[:：|『』｜|，|｜()（）？?\\。\/]/g, '-')
+}
+
+// 格式化时间
+const formatTime = (ms) => {
+    let time = new Date(0, 0, 0, 0, 0, 0, ms)
+    let minute = time.getMinutes() <= 10 ? '0' + time.getMinutes() : time.getMinutes()
+    let second = time.getSeconds() <= 10 ? '0' + time.getSeconds() : time.getSeconds()
+    return minute + ':' + second
 }
 
 // setTimeout( () => {
